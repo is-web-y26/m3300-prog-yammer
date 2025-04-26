b1ca9e96d2886d92f2ce216b8f19ace0198fc110
prog.yammer <prog-yammer@yandex.ru>
2025-04-26 14:07:09 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import * as methodOverride from 'method-override';
 import * as exphbs from 'express-handlebars';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { NotFoundExceptionFilter } from './filters/not_found_exc.filter';
-import { engine } from 'express-handlebars';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -18,8 +16,9 @@ async function bootstrap() {
     layoutsDir: join(__dirname, '..', 'views', 'layouts'),
     partialsDir: join(__dirname, '..', 'views', 'partials'),
     helpers: {
-      isActiveNavItem: (current, route) => current === route ? ' nav__link--active' : ''
-    }
+      isActiveNavItem: (current, route) =>
+        current === route ? ' nav__link--active' : '',
+    },
   });
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
@@ -27,12 +26,12 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
-  app.use(
-    methodOverride('_method', {
-      methods: ['POST', 'GET'],
-    }),
-  );
-  app.use(methodOverride('_method'));
+  // app.use(
+  //   methodOverride('_method', {
+  //     methods: ['POST', 'GET'],
+  //   }),
+  // );
+  // app.use(methodOverride('_method'));
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
